36c232faf45caa619177f873bee58f6c7af15969
Vladislav Jevremović <vladislav@fastmail.com>
2019-11-14 08:52:21 +0100

    Initial Commit

---
+++ WebcamViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebcamViewer
+//
+//  Created by Vladislav Jevremovic on 14/11/2019.
+//  Copyright © 2019 Vladislav Jevremovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
